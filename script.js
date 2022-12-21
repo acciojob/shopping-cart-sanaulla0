c6b4a5dca54911f6baa6580c24bb85c23825c02b
sanaulla0 <vadapallisanaulla@gmail.com>
2022-12-21 13:18:46 +0530

    p1

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
 //your code here
+let  itemInput = document.getElementById('item-name-input');
+let priceInput = document.getElementById('item-price-input');
+let button = document.getElementById('add');
+let table = document.getElementById('table');
+let total = document.getElementById('total');
+
+// let priceArr = [];
+let sum =0;
+function addItem(){
+
+total.innerText=2;
+let item = itemInput.value;
+let price = priceInput.value;
+
+if(item !=='' && price !==""){
+ 
+ let row = document.createElement('tr');
+ row.innerText='';
+ // priceArr.push(Number(price));
+ sum+=Number(price);
+ // console.log(priceArr);
+ console.log(sum);
+
+row.innerHTML =`<td>${item} </td> <td>${price}</td>`
+
+// for(let i=0;i < priceArr.length;i++){
+//   sum += priceArr[i];
+// }
+console.log(sum);
+table.append(row);
+total.innerText = sum;
+
+itemInput.value='';
+priceInput.value='';
+
+
+}
+
+
+
+}
+button.onclick=addItem;
